2d069d72c7a5f007e9c08030cea5b8af0553f94f
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-06-27 03:08:33 -0300

    EventControllerIT update OK

--- src/main/java/com/devraphael/desafio/controllers/EventController.java
+++ src/main/java/com/devraphael/desafio/controllers/EventController.java
@@ -1,9 +1,14 @@
 package com.devraphael.desafio.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devraphael.desafio.dto.EventDTO;
 import com.devraphael.desafio.services.EventService;
 
 @RestController
@@ -12,4 +17,10 @@ public class EventController {
 
 	@Autowired
 	private EventService service;
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<EventDTO> update(@PathVariable Long id, @RequestBody EventDTO dto) {
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().body(dto);
+	}
 }
--- src/main/java/com/devraphael/desafio/services/EventService.java
+++ src/main/java/com/devraphael/desafio/services/EventService.java
@@ -2,8 +2,15 @@ package com.devraphael.desafio.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devraphael.desafio.dto.EventDTO;
+import com.devraphael.desafio.entities.City;
+import com.devraphael.desafio.entities.Event;
+import com.devraphael.desafio.exceptions.ResourceNotFoundException;
 import com.devraphael.desafio.repository.EventRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class EventService {
@@ -11,4 +18,19 @@ public class EventService {
 	@Autowired
 	private EventRepository repository;
 	
+	
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+		try {
+			Event entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity.setDate(dto.getDate());
+			entity.setUrl(dto.getUrl());
+			entity.setCity(new City(dto.getCityId(), null));
+			entity = repository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
 }
