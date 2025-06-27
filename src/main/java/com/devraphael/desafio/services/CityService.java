0da2b23227f7cd48ec8c556240dc9af55a69aba8
Raphael Eduardo <raphaeleduardosc@gmail.com>
2025-06-27 02:23:15 -0300

    CityControllerIT insert OK

--- src/main/java/com/devraphael/desafio/services/CityService.java
+++ src/main/java/com/devraphael/desafio/services/CityService.java
@@ -6,8 +6,10 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devraphael.desafio.dto.CityDTO;
+import com.devraphael.desafio.entities.City;
 import com.devraphael.desafio.repository.CityRepository;
 
 @Service
@@ -16,8 +18,17 @@ public class CityService {
 	@Autowired
 	private CityRepository repository;
 	
+	@Transactional(readOnly = true)
 	public List<CityDTO> findAll() {
 		return repository.findAll(Sort.by("name")).stream()
 				.map(x -> new CityDTO(x)).collect(Collectors.toList());
+	}
+	
+	@Transactional
+	public CityDTO insert(CityDTO dto) {
+		City entity = new City();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		return new CityDTO(entity);	
 	}
 }
